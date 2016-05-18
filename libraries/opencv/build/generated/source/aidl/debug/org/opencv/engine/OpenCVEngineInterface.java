e29ef3ca1e80fe1143049fac0908bb52004fbc10
Vijay Kumar <vijaykumar@ucdavis.edu>
2016-05-17 23:39:46 -0700

    removed submenu from navigation drawer

--- libraries/opencv/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ libraries/opencv/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\Smart\\AndroidStudioProjects\\ChromeLED\\libraries\\opencv\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: /Users/vijay/AndroidStudioProjects/ChromeLED/libraries/opencv/src/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
