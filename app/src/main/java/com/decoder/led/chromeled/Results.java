32caee68567e94a1857fe4a17dea45a7af228dc3
vincenttrinh <vincenttrinh94@gmail.com>
2016-05-25 18:59:25 -0700

    movable ROI

--- app/src/main/java/com/decoder/led/chromeled/Results.java
+++ app/src/main/java/com/decoder/led/chromeled/Results.java
@@ -1,8 +1,12 @@
 package com.decoder.led.chromeled;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.lang.String;
+import java.util.List;
+
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +15,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 import org.w3c.dom.Text;
 
 
@@ -22,6 +30,7 @@ public class Results extends AppCompatActivity {
         setContentView(R.layout.activity_results);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         HashMap<String, String> key = new HashMap<String, String>();
         key.put("0000", "0");
         key.put("0001", "1");
@@ -40,119 +49,39 @@ public class Results extends AppCompatActivity {
         key.put("1110", "E");
         key.put("1111", "F");
 
-
         //bundle setup, refer between results activity and tutorial1activity
         Bundle extras = getIntent().getExtras();
-        int[] receivedData = extras.getIntArray("Array");
-        //int[]
-        String signalValues = extras.getString("signalIn");
         TextView tv = (TextView)findViewById(R.id.textView_activity_results);
-        TextView tv2 = (TextView)findViewById(R.id.bugInfo);
-        String bitS = extras.getString("bitString");
-        //tv.setText(extras.getString("Message"));
-        String debugOut = "Signal length: " + signalValues.length() + '\n' + "Some bytes: ";
-        debugOut += signalValues.substring(0, 64);
-        //int[] signalBinary = valuetoBinary(signalValues);
-
-        String out = "Message in Hex: " + msgDecode(signalValues, key);
-        //String out = msgDecode(receivedData, key);
-        tv2.setText(debugOut);
-        tv.setText(out);
-
-
+        tv.setText(processImages(extras.getLongArray("times"), extras.getInt("timeIndex"), extras.getBoolean("missedFrame"), extras.getIntArray("missedFrames"), extras.getInt("missedFrameIndex")));
     }
 
-    /*public int[] valuetoBinary(int[] signalValues){
-        int[] signalBinary;
+    String processImages(long[] times, int numTimes, boolean missedFrame, int[] missedFrames, int missedFrameIndex) {
+        String retString = "";
 
-        return signalBinary;
-    }
-    */
-
-    public String msgDecode(String bitString, HashMap<String, String> key ){
-        //start here
-        String output = "We processed!";
-        String buffer = null;
-        String buffer2 = null;
-        String hexTodecimal = null;
-        String outMsg = "";
-        int decimal = 0;
-        String decMsg = "";
-        int j = 0;
-        int modLength = 0;
-        int bitLength = bitString.length();
-
-        //value -> binary
-        /*for(int i = 0; i < bitLength; i++){
-            if(bitString[i] > 0)
-                bitString[i] = 1;
-            else
-                bitString[i] = 0;
-            //if 0 or -x, set to zero for whatever reason we get a negative number
-        }*/
-
-        //bitString contains our binary input,
-        //decoding process
-        modLength = bitLength % 4;
-        if(bitLength > 0) {
-            if(bitLength < 4) {
-                Log.i(TAG, "short input , is " + bitString.length() + " end");
-                return "Short input";
-                //will bitString.length be same as bitLength? during our process
-            }
-            else
-            //add a input.length mod 4 and create while loop w/ end condition as input.length - remainder cutting off last bits
-            //add checker to see if high or low, if value exist then yes
-            //for i -> 256, take subarry of bitString of size 4 and pull from hex table.
-
-
-            for (int i = 0; i < (bitLength - modLength); i += 4) {
-                //for int array
-                //buffer = Integer.toString(bitString[i]) + Integer.toString(bitString[i + 1]) + Integer.toString(bitString[i + 2]) + Integer.toString(bitString[i + 3]);
-
-                //code to convert hex to ascii ( might not actually be used because of code below)
-                //buffer2 =  bitString.substring(i+4, i+8);
-                //hexTodecimal += key.get(buffer);
-                //hexTodecimal += key.get(buffer2);
-                //decMsg += key.get(buffer); //replace with buffer when working
-                //decMsg += key.get(buffer2);
-                //decimal = Integer.parseInt(hexTodecimal, 16);
-
-                //** Code for String of bits input **
-                //decMsg contains a string of hex characters from byte
-                buffer = bitString.substring(i, i+4);
-                decMsg += key.get(buffer);
-                //decMsg contains Hex
+        if (missedFrame) {
+            for (int i=0; i < missedFrameIndex; i++) {
+                Log.i(TAG, "Missed frame " + missedFrames[i]);
             }
         }
-        //without ascii conversion ( prints decMsg )
-        //outMsg = decMsg;
 
-        //convert decMsg to ASCII
-        while(j < decMsg.length()){
-            //check for escape char 0xDB
-            if(decMsg.substring(j,j+2) == "DB") {
-                j+=2;
-                if(decMsg.substring(j+2, j+4) == "50")
-                    outMsg += "C0";
-                else if(decMsg.substring(j+2, j+4) == "51")
-                    outMsg += "AA";
-                else if(decMsg.substring(j+2, j+4) == "52")
-                    outMsg += "DB";
-                else
-                    outMsg += "??";
+        for (int i=0; i < numTimes; i++) {
+            long myTimestamp = times[i];
+
+            Mat mHierarchy = new Mat();
+            List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+            Scalar CONTOUR_COLOR = new Scalar(0,255,0);
+
+            if (DetectActivity.images.size() != 0) {
+                Mat myMat = DetectActivity.images.get(i);
+                Imgproc.findContours(myMat, contours, mHierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
             }
+
+            if(contours.size() > 0)
+                retString += "1";
             else
-                outMsg += decMsg.substring(j,j+2);
-
-            //covert 1 byte into ascii
-            //decimal = Integer.parseInt(decMsg.substring(j, j+2), 16);
-            //outMsg += ((char) decimal);
-            j+=2;
+                retString += "0";
+            Log.i(TAG, "retString = " + retString + ", times[" + i + "] = " + times[i] + ", numContours = " + contours.size());
         }
-
-        return outMsg;
-        //add error handling
+        return retString;
     }
-
 }
